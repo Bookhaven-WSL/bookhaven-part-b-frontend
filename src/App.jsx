6fe45e32a9e08fb97d5837fb5022a4f0d807ff4a
Wade <158378370+wadevenz@users.noreply.github.com>
2024-12-17 21:09:24 +1000

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css'
 import ReadPage from './pages/Read'
 import ToBeReadPage from './pages/ToBeReadPage'
@@ -7,14 +7,17 @@ import Recommendations from './pages/Recommendations'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
 import LoginSignupPage from './pages/LoginSignupPage'
+import AuthProvider from "./contexts/authProvider";
+import Routes from './routes/index.jsx'
+
 
 
 function App() {
 
   return (
-    <>
-      <LoginSignupPage />
-    </>
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
   )
 }
 
