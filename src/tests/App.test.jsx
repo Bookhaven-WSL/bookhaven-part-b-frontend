465f487cf4d4ecb52ebdecdbfa57a74d8b100940
Louis Denman <lmdenman02@gmail.com>
2024-12-19 17:38:18 +1000

    made small changes to App test

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,10 +1,19 @@
-import { describe, it, expect } from 'vitest'
+import { describe, test, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import App from ".././App.jsx"
 
 describe('App', () => {
-    it('renders the App component', () => {
+    test('Render the App component and ensure elements are there', () => {
       render(<App />)
+
+      const bookhavenTitle = screen.getByText("BOOKHAVEN")
+      expect(bookhavenTitle).toBeInTheDocument()
+
+      const signup = screen.getByText("SIGNUP")
+      expect(signup).toBeInTheDocument()
+
+      const login = screen.getByText("LOGIN")
+      expect(login).toBeInTheDocument()
       
       screen.debug()
     })
