07131ac1f0d93837bdee6b009c00dcaf3bbcb2f6
Wade <158378370+wadevenz@users.noreply.github.com>
2024-12-20 14:31:01 +1000

    feat: cards add buttons functionality fixed to remove text on click

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { addBookRead, addBookToBeRead } from "../ApiFunctionality/ApiFunctions";
 import "../styles/Cards.css";
 
@@ -6,36 +7,61 @@ const Cards = ({ books, className }) => {
         return <div className={className}>No books found.</div>;
     }
 
+
+    const [buttonReadText, setbuttonReadText] = useState(
+        books.map(() => ({text: "Read", disabled: false}))
+    );
+
+    const [buttonToBeReadText, setbuttonToBeReadText] = useState(
+        books.map(() => ({text: "Want To Read", disabled: false}))
+    );
+
+    const handleButton = (index) => {
+        const updatedReadStates = [...buttonReadText];
+        const updatedToBeReadStates = [...buttonToBeReadText];
+        updatedReadStates[index] = { text: "", disabled: true};
+        updatedToBeReadStates[index] = { text: "", disabled: true};
+        setbuttonReadText(updatedReadStates);
+        setbuttonToBeReadText(updatedToBeReadStates);
+    }
+
     return (
         <div className={className}>
             {books.map((bookArray, index) => {
                 return (
-                    <div key={index} className="card">
-                        <img
-                            src={bookArray[5].coverImage}
-                            alt={`Book Cover ${index + 1}`}
-                            className="card-book-img"
-                        />
-                        <button 
-                            className="read-button"
-                            onClick={() => { 
-                                if (bookArray[0]?.olid) {
-                                    addBookRead(bookArray[0].olid);
-                                } else {
-                                    console.error('Invalid OLID');
-                                }
-                            }}>Read</button>
-                            
-                        <button 
-                            className="tbr-button"
-                            onClick={() => { 
-                                if (bookArray[0]?.olid) {
-                                    addBookToBeRead(bookArray[0].olid);
-                                } else {
-                                    console.error('Invalid OLID');
-                                }
-                            }}>Want To Read</button>
-                    </div>   
+                    <>
+                        <div key={index} className="card">
+                            <img
+                                src={bookArray[5].coverImage}
+                                alt={`Book Cover ${index + 1}`}
+                                className="card-book-img"
+                            />
+                            <button 
+                                className="read-button"
+                                onClick={() => { 
+                                    if (bookArray[0]?.olid) {
+                                        addBookRead(bookArray[0].olid); handleButton(index);
+                                    } else {
+                                        console.error('Invalid OLID');
+                                    }
+                                }}
+                                disabled={buttonReadText[index].disabled}
+                                >{buttonReadText[index].text}</button>
+                                
+                            <button 
+                                className="tbr-button"
+                                onClick={() => { 
+                                    if (bookArray[0]?.olid) {
+                                        addBookToBeRead(bookArray[0].olid); handleButton(index);
+                                    } else {
+                                        console.error('Invalid OLID');
+                                    }
+                                }}
+                                disabled={buttonToBeReadText[index].disabled}
+                                >{buttonToBeReadText[index].text}</button>
+                        </div> 
+                    </>
+                  
                 );
             })}
         </div>
